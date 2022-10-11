8f445cb8583b21a2b60f125f3a0cdc007d1124f7
Aydin Pramasta <pramastaaydin@gmail.com>
2022-10-11 12:19:38 +0700

    remove unused code

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-require __DIR__ . '/auth.php';
+Route::middleware('guest')->group(function () {
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+});
 
 Route::middleware('auth')->group(function () {
+    Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+
     Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
