aa0567cbd51aaf22ea55b550dd1de65f7956db23
Aydin Pramasta <pramastaaydin@gmail.com>
2022-10-13 12:08:00 +0700

    register:admin command

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,42 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('register:admin', function () {
+    $credentials['name'] = $this->ask('Name');
+    $credentials['username'] = $this->ask('Username');
+    $credentials['role'] = 'ADMIN';
+
+    $credentials['password'] = $this->secret('Password');
+    $credentials['confirm_password'] = $this->secret('Confirm Password');
+
+    while (!(strlen($credentials['password']) > 6 && $credentials['password'] === $credentials['confirm_password'])) {
+        if (strlen($credentials['password']) <= 6) {
+            $this->error('Password must be more than six characters');
+        }
+
+        if ($credentials['password'] !== $credentials['confirm_password']) {
+            $this->error('Password and Confirm password do not match');
+        }
+
+        $credentials['password'] = $this->secret('Password');
+        $credentials['confirm_password'] = $this->secret('Confirm Password');
+    }
+
+    $credentials['password'] = bcrypt($credentials['password']);
+
+    $admin = User::create($credentials);
+
+    $headers = ['Name', 'Username', 'Admin'];
+
+    $fields = [
+        'Name' => $admin->name,
+        'Username' => $admin->username,
+        'Admin' => $admin->role === 'ADMIN'
+            ? 'true' : 'false'
+    ];
+
+    $this->info('Admin created!');
+
+    $this->table($headers, [$fields]);
+})->purpose('Register an Admin user');
