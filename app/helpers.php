090292f6d670b7b8abc129744eadebae4b56416c
Aydin Pramasta <aydin.pram@gmail.com>
2023-03-18 21:22:29 +0700

    create helper function to hash and verify Django password

---
+++ app/helpers.php
@@ -0,0 +1,61 @@
+<?php
+
+if (!function_exists('django_password_verify')) {
+    /**
+     * Verify a Django password (PBKDF2-SHA256)
+     *
+     * @ref http://stackoverflow.com/a/39311299/2224584
+     * @param string $password   The password provided by the user
+     * @param string $djangoHash The hash stored in the Django app
+     * @return bool
+     * @throws Exception
+     */
+    function django_password_verify(string $password, string $djangoHash): bool
+    {
+        $pieces = explode('$', $djangoHash);
+        if (count($pieces) !== 4) {
+            return false;
+        }
+        list($header, $iter, $salt, $hash) = $pieces;
+        // Get the hash algorithm used:
+        if (preg_match('#^pbkdf2_([a-z0-9A-Z]+)$#', $header, $m)) {
+            $algo = $m[1];
+        } else {
+            return false;
+        }
+        if (!in_array($algo, hash_algos())) {
+            return false;
+        }
+
+        $calc = hash_pbkdf2(
+            $algo,
+            $password,
+            $salt,
+            (int) $iter,
+            32,
+            true
+        );
+        return hash_equals($calc, base64_decode($hash));
+    }
+}
+
+if (!function_exists("django_password_hash")) {
+    /**
+     * Hash password to Django style (PBKDF2-SHA256)
+     *
+     * @param string $password  The password provided by the user
+     * @param string $salt      Cryptographically secure random string
+     */
+    function django_password_hash(string $password, string $salt): string {
+        $algo = 'sha256';
+        $iterations = 260000;
+        $hash = base64_encode(hash_pbkdf2($algo, $password, $salt, $iterations, 32, true));
+
+        return implode('$', [
+           'pbkdf2_' . $algo,
+           $iterations,
+           $salt,
+           $hash
+        ]);
+    }
+}
